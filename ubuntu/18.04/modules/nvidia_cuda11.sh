543bf31312340237d2721d8eb5f6d2698cad0eb6
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-06-29 15:49:44 +0900

    Fix version

--- ubuntu/18.04/modules/nvidia_cuda11.sh
+++ ubuntu/18.04/modules/nvidia_cuda11.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-sudo apt-mark unhold cuda cuda-drivers cuda-toolkit-config-common nvidia-modprobe nvidia-settings
+sudo apt-mark unhold \
+cuda \
+cuda-drivers \
+cuda-drivers-470 \
+cuda-toolkit-config-common \
+nvidia-modprobe \
+nvidia-settings \
+libxnvctrl0 \
+cuda-toolkit-11-config-common
+
 #sudo aptitude purge ~ncuda
-sudo apt purge -y --allow-change-held-packages "*cuda*" "*cudnn*" "*nvidia*" "*nsight*" "libcublas*"
+sudo apt purge -y --allow-change-held-packages \
+"libxnvctrl0" "*cuda*" "*cudnn*" "*nvidia*" "*nsight*" "libcublas*"
+
 sudo apt autoremove -y
 
 mkdir -p ~/lib/nvidia
@@ -11,12 +22,26 @@ cd ~/lib/nvidia
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt update
-sudo apt install -y cuda=11.4.4-1 \
-cuda-drivers=470.199.02-1 \
+
+sudo apt install -y --allow-downgrades \
+cuda=11.4.4-1 \
+cuda-drivers=470.182.03-1 \
+cuda-drivers-470=470.182.03-1 \
 cuda-toolkit-config-common=11.4.148-1 \
-nvidia-modprobe=470.199.02-0ubuntu1 \
-nvidia-settings=470.199.02-0ubuntu1
-sudo apt-mark hold cuda cuda-drivers cuda-toolkit-config-common nvidia-modprobe nvidia-settings
+cuda-toolkit-11-config-common=11.4.148-1 \
+nvidia-modprobe=470.182.03-0ubuntu1 \
+nvidia-settings=470.182.03-0ubuntu1 \
+libxnvctrl0=470.182.03-0ubuntu1
+
+sudo apt-mark hold \
+cuda \
+cuda-drivers \
+cuda-drivers-470 \
+cuda-toolkit-config-common \
+nvidia-modprobe \
+nvidia-settings \
+libxnvctrl0 \
+cuda-toolkit-11-config-common
 
 #create symbolic link for cublas (for gazr)
 # sudo ln -s /usr/lib/x86_64-linux-gnu/libcublas.so /usr/local/cuda-11.4/lib64/libcublas.so
