cdba6d554c0460c7be2de657c67ba4b78f667a21
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-07-03 15:15:48 +0900

    Fix version

--- ubuntu/18.04/modules/nvidia_cuda11.sh
+++ ubuntu/18.04/modules/nvidia_cuda11.sh
@@ -19,7 +19,12 @@ sudo apt autoremove -y
 mkdir -p ~/lib/nvidia
 cd ~/lib/nvidia
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+if [ -f "cuda-keyring_1.0-1_all.deb" ]; then
+    echo "cuda-keyring_1.0-1_all.deb exists."
+else
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+fi
+
 sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo apt update
 
@@ -29,8 +34,24 @@ cuda-drivers=470.182.03-1 \
 cuda-drivers-470=470.182.03-1 \
 cuda-toolkit-config-common=11.4.148-1 \
 cuda-toolkit-11-config-common=11.4.148-1 \
+libnvidia-cfg1-470=470.182.03-0ubuntu1 \
+libnvidia-common-470=470.182.03-0ubuntu1 \
+libnvidia-compute-470=470.182.03-0ubuntu1 \
+libnvidia-decode-470=470.182.03-0ubuntu1 \
+libnvidia-encode-470=470.182.03-0ubuntu1 \
+libnvidia-extra-470=470.182.03-0ubuntu1 \
+libnvidia-fbc1-470=470.182.03-0ubuntu1 \
+libnvidia-gl-470=470.182.03-0ubuntu1 \
+libnvidia-ifr1-470=470.182.03-0ubuntu1 \
+nvidia-compute-utils-470=470.182.03-0ubuntu1 \
+nvidia-dkms-470=470.182.03-0ubuntu1 \
+nvidia-driver-470=470.182.03-0ubuntu1 \
+nvidia-kernel-common-470=470.182.03-0ubuntu1 \
+nvidia-kernel-source-470=470.182.03-0ubuntu1 \
 nvidia-modprobe=470.182.03-0ubuntu1 \
 nvidia-settings=470.182.03-0ubuntu1 \
+nvidia-utils-470=470.182.03-0ubuntu1 \
+xserver-xorg-video-nvidia-470=470.182.03-0ubuntu1 \
 libxnvctrl0=470.182.03-0ubuntu1
 
 sudo apt-mark hold \
@@ -38,10 +59,26 @@ cuda \
 cuda-drivers \
 cuda-drivers-470 \
 cuda-toolkit-config-common \
+cuda-toolkit-11-config-common \
+libnvidia-cfg1-470 \
+libnvidia-common-470 \
+libnvidia-compute-470 \
+libnvidia-decode-470 \
+libnvidia-encode-470 \
+libnvidia-extra-470 \
+libnvidia-fbc1-470 \
+libnvidia-gl-470 \
+libnvidia-ifr1-470 \
+nvidia-compute-utils-470 \
+nvidia-dkms-470 \
+nvidia-driver-470 \
+nvidia-kernel-common-470 \
+nvidia-kernel-source-470 \
 nvidia-modprobe \
 nvidia-settings \
-libxnvctrl0 \
-cuda-toolkit-11-config-common
+nvidia-utils-470 \
+xserver-xorg-video-nvidia-470 \
+libxnvctrl0
 
 #create symbolic link for cublas (for gazr)
 # sudo ln -s /usr/lib/x86_64-linux-gnu/libcublas.so /usr/local/cuda-11.4/lib64/libcublas.so
