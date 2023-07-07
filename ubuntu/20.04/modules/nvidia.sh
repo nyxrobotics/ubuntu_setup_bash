c2b9081e08bdcad4b874f83aeba146836c081682
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2023-07-07 19:49:57 +0900

    Use cuda_keyring

--- ubuntu/20.04/modules/nvidia.sh
+++ ubuntu/20.04/modules/nvidia.sh
@@ -1,27 +1,85 @@
 #!/bin/bash
 
-sudo apt-mark unhold cuda-drivers
+sudo apt-mark unhold \
+cuda \
+cuda-drivers \
+cuda-drivers-470 \
+cuda-toolkit-config-common \
+nvidia-modprobe \
+nvidia-settings \
+libxnvctrl0 \
+cuda-toolkit-11-config-common
+
 #sudo aptitude purge ~ncuda
-sudo apt purge -y --allow-change-held-packages "*cuda*" "*nvidia*" "*nsight*" "libcublas*"
+sudo apt purge -y --allow-change-held-packages \
+"libxnvctrl0" "*cuda*" "*cudnn*" "*nvidia*" "*nsight*" "libcublas*"
+
 sudo apt autoremove -y
 
 mkdir -p ~/lib/nvidia
 cd ~/lib/nvidia
 
-if [ -f "cuda-repo-ubuntu2004-11-4-local_11.4.4-470.82.01-1_amd64.deb" ]; then
-    echo "cuda-repo-ubuntu2004-11-4-local_11.4.4-470.82.01-1_amd64.deb exists."
+if [ -f "cuda-keyring_1.1-1_all.deb" ]; then
+    echo "cuda-keyring_1.1-1_all.deb exists."
 else
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-    sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget https://developer.download.nvidia.com/compute/cuda/11.4.4/local_installers/cuda-repo-ubuntu2004-11-4-local_11.4.4-470.82.01-1_amd64.deb
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
 fi
 
-sudo dpkg -i cuda-repo-ubuntu2004-11-4-local_11.4.4-470.82.01-1_amd64.deb
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/7fa2af80.pub
-sudo apt-key add /var/cuda-repo-ubuntu2004-11-4-local/3bf863cc.pub
+sudo dpkg -i cuda-keyring_1.1-1_all.deb
 sudo apt update
-sudo apt --only-upgrade install cuda-repo-ubuntu2004-11-4-local
-sudo apt install -y cuda
+
+sudo apt install -y --allow-downgrades \
+cuda=11.4.4-1 \
+cuda-drivers=470.182.03-1 \
+cuda-drivers-470=470.182.03-1 \
+cuda-toolkit-config-common=11.4.148-1 \
+cuda-toolkit-11-config-common=11.4.148-1 \
+libnvidia-cfg1-470=470.182.03-0ubuntu1 \
+libnvidia-common-470=470.182.03-0ubuntu1 \
+libnvidia-compute-470=470.182.03-0ubuntu1 \
+libnvidia-decode-470=470.182.03-0ubuntu1 \
+libnvidia-encode-470=470.182.03-0ubuntu1 \
+libnvidia-extra-470=470.182.03-0ubuntu1 \
+libnvidia-fbc1-470=470.182.03-0ubuntu1 \
+libnvidia-gl-470=470.182.03-0ubuntu1 \
+libnvidia-ifr1-470=470.182.03-0ubuntu1 \
+nvidia-compute-utils-470=470.182.03-0ubuntu1 \
+nvidia-dkms-470=470.182.03-0ubuntu1 \
+nvidia-driver-470=470.182.03-0ubuntu1 \
+nvidia-kernel-common-470=470.182.03-0ubuntu1 \
+nvidia-kernel-source-470=470.182.03-0ubuntu1 \
+nvidia-modprobe=470.182.03-0ubuntu1 \
+nvidia-settings=470.182.03-0ubuntu1 \
+nvidia-utils-470=470.182.03-0ubuntu1 \
+xserver-xorg-video-nvidia-470=470.182.03-0ubuntu1 \
+libxnvctrl0=470.182.03-0ubuntu1
+
+sudo apt-mark hold \
+cuda \
+cuda-drivers \
+cuda-drivers-470 \
+cuda-toolkit-config-common \
+cuda-toolkit-11-config-common \
+libnvidia-cfg1-470 \
+libnvidia-common-470 \
+libnvidia-compute-470 \
+libnvidia-decode-470 \
+libnvidia-encode-470 \
+libnvidia-extra-470 \
+libnvidia-fbc1-470 \
+libnvidia-gl-470 \
+libnvidia-ifr1-470 \
+nvidia-compute-utils-470 \
+nvidia-dkms-470 \
+nvidia-driver-470 \
+nvidia-kernel-common-470 \
+nvidia-kernel-source-470 \
+nvidia-modprobe \
+nvidia-settings \
+nvidia-utils-470 \
+xserver-xorg-video-nvidia-470 \
+libxnvctrl0
+
 #create symbolic link for cublas (for gazr)
 # sudo ln -s /usr/lib/x86_64-linux-gnu/libcublas.so /usr/local/cuda-11.4/lib64/libcublas.so
 if ! grep -Fxq "## CUDA and cuDNN paths" ~/.bashrc
