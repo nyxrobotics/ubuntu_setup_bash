db08fecb5ae780367f858f63d64b809d4d480113
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2024-01-12 12:52:34 +0900

    Add script installing azure kinect

---
+++ ubuntu/20.04/modules/azure_kinect.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Reference: https://github.com/juancarlosmiranda/azure_kinect_notes
+# sudo rm /etc/apt/trusted.gpg.d/microsoft.gpg
+curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+# curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
+sudo apt-add-repository "deb [arch=amd64,arm64,armhf] https://packages.microsoft.com/ubuntu/$(lsb_release -rs)/prod $(lsb_release -cs) main"
+echo 'libk4a1.4 libk4a1.4/accepted-eula-hash string 0f5d5c5de396e4fee4c0753a21fee0c1ed726cf0316204edda484f08cb266d76' | sudo debconf-set-selections
+echo 'libk4a1.4 libk4a1.4/accept-eula boolean true' | sudo debconf-set-selections
+sudo apt update && sudo apt -y install libk4a1.4 libk4a1.4-dev k4a-tools
