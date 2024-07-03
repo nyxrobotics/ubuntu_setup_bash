913300ce097ac6858ffd0da8967b191177932802
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2024-07-03 14:41:48 +0900

    Fix hstr

--- ubuntu/20.04/modules/hstr.sh
+++ ubuntu/20.04/modules/hstr.sh
@@ -2,9 +2,8 @@
 
 ### HSTR
 ### Reference: https://unix.stackexchange.com/questions/209495/bash-incremental-history-search-ctrlr-matching-multiple-non-adjacent-word
-wget -qO - https://www.mindforger.com/gpgpubkey.txt | sudo apt-key add -
 # add PPA to APT sources:
-echo "deb http://www.mindforger.com/debian-ppa/bookworm bookworm main" | sudo tee /etc/apt/sources.list.d/mindforger.list
+sudo add-apt-repository ppa:ultradvorka/ppa
 sudo apt update
 sudo apt install hstr
 
