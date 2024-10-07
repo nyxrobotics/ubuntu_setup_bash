82e9894afe87dc76f9783020be0803cc18741990
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2024-10-07 16:04:52 +1300

    Fix dark theme

--- ubuntu/24.04/modules/dark_theme.sh
+++ ubuntu/24.04/modules/dark_theme.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# sudo apt install gnome-tweak-tool libglib2.0-bin
+### Reference: https://github.com/littleant/auto-darkmode-switcher/tree/main
+
+# sudo apt install gnome-tweaks libglib2.0-bin
 
 ### Dark theme
 gsettings set org.gnome.desktop.interface gtk-theme "Yaru-dark"
+gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+
 ### Default theme
 # gsettings set org.gnome.desktop.interface gtk-theme "Yaru"
+# gsettings set org.gnome.desktop.interface color-scheme prefer-light
