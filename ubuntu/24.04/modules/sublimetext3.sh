c02d220382e48742310c50ca51aee082a0f52add
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2024-10-07 15:55:45 +1300

    Fix sublime text installation

--- ubuntu/24.04/modules/sublimetext3.sh
+++ ubuntu/24.04/modules/sublimetext3.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+### Reference: https://www.sublimetext.com/docs/linux_repositories.html
 # >>>sublime text3
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
 sudo add-apt-repository -y "deb https://download.sublimetext.com/ apt/stable/"
 sudo apt update
 sudo apt install -y sublime-text
