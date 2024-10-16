e5984ff52bb5a80cfd60b25ca21625726aa093b4
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2024-10-16 16:31:45 +1300

    Fix checkinstall installation

--- ubuntu/24.04/modules/checkinstall.sh
+++ ubuntu/24.04/modules/checkinstall.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-sudo apt install -y checkinstall
+sudo apt install -y checkinstall gettext
 mkdir -p ~/lib
 cd ~/lib
-git clone git@github.com:giuliomoro/checkinstall.git
+git clone -b feature/ubuntu24.04 git@github.com:nyxrobotics/checkinstall.git
 cd checkinstall
-./configure
 make
-sudo checkinstall -yD --pkgname=checkinstall --pkgversion=1.6.3
+sudo checkinstall -yD --install=no --fstrans=no --pkgname=checkinstall --pkgversion=1.6.3 
 sudo apt purge -y checkinstall
 sudo apt autoremove -y
 sudo dpkg -i checkinstall_1.6.3-1_amd64.deb
