69ecc121fa2ac4b3320ac766eec1e672d974edc5
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2024-12-12 12:16:37 +0900

    Add virtualbox installation

---
+++ ubuntu/20.04/modules/virtualbox.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ~/lib/virtualbox
+cd ~/lib/virtualbox
+wget https://download.virtualbox.org/virtualbox/7.1.4/virtualbox-7.1_7.1.4-165100~Ubuntu~focal_amd64.deb
+sudo dpkg -i virtualbox-7.1_7.1.4-165100~Ubuntu~focal_amd64.deb
+
+# sudo checkinstall --fstrans=no -yD --pkgname=virtualbox-symbolic-links --pkgversion=1 bash -c "ln -s /usr/lib/virtualbox/libdl.so /usr/lib/libdl.so.2; ln -s /usr/lib/virtualbox/libpthread.so /usr/lib/libpthread.so.0;"
+# sudo dpkg -i virtualbox-symbolic-links_1-1_amd64.deb
