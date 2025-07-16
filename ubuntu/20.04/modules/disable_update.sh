5b18b0c8922421ccbff1df6621961924d6f39e77
Takaaki Numai <takaaki.numai01@g.softbank.co.jp>
2025-07-16 11:41:20 +0900

    Update disable_update.sh

--- ubuntu/20.04/modules/disable_update.sh
+++ ubuntu/20.04/modules/disable_update.sh
@@ -4,8 +4,33 @@ SCRIPT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 cd $SCRIPT_DIR
 
 # >>> Disable update
+# Services
 sudo systemctl disable --now unattended-upgrades
+sudo systemctl disable --now apt-daily.service apt-daily-upgrade.service
+sudo systemctl disable --now apt-daily.timer apt-daily-upgrade.timer
+sudo systemctl disable --now snapd.socket
+sudo systemctl disable --now snapd
 sudo systemctl stop unattended-upgrades
+sudo systemctl stop apt-daily.service apt-daily-upgrade.service
+sudo systemctl stop apt-daily.timer apt-daily-upgrade.timer
+sudo systemctl stop snapd.socket
+sudo systemctl stop snapd
+
+# Gsettings
 gsettings set com.ubuntu.update-notifier hide-reboot-notification true
-gconftool -s --type bool /apps/update-notifier/auto_launch false
-sudo mv /etc/xdg/autostart/update-notifier.desktop /etc/xdg/autostart/update-notifier.desktop.back
+gsettings set com.ubuntu.update-notifier no-show-notifications true
+gsettings set org.gnome.desktop.notifications show-banners false
+
+# Snap
+# snap refresh --hold
+
+# Desktop
+# gconftool -s --type bool /apps/update-notifier/auto_launch false
+# sudo mv /etc/xdg/autostart/update-notifier.desktop /etc/xdg/autostart/update-notifier.desktop.back
+
+# Remove gnome packages
+sudo apt remove --purge -y unattended-upgrades gnome-software-plugin-snap
+sudo rm /var/log/unattended-upgrades/*
+
+# sudo snap remove $(snap list | awk ' !/^Name|^core/ {print $1}')
+# sudo apt remove --purge -y snapd
