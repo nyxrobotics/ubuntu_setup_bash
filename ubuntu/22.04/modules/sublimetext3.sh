9ee5f2af16e655c0c51e53b8f51c5b043c84a869
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2025-10-08 18:36:41 +0900

    Fix sublime text on 22.04

--- ubuntu/22.04/modules/sublimetext3.sh
+++ ubuntu/22.04/modules/sublimetext3.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+## Reference: https://linuxcapable.com/how-to-install-sublime-text-on-ubuntu-linux/
+
 # >>>sublime text3
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
-echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+curl -fSsL https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/sublimehq-pub.gpg > /dev/null
+echo 'deb [signed-by=/usr/share/keyrings/sublimehq-pub.gpg] https://download.sublimetext.com/ apt/stable/' | sudo tee -a /etc/apt/sources.list.d/sublime-text.list
 sudo apt update
 sudo apt install -y sublime-text
 
