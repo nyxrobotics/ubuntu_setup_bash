ac4318569a640212f9d5b9710aad71771829c4bd
TakaakiNumai <takaaki.numai01@g.softbank.co.jp>
2025-10-08 15:18:47 +0900

    (WIP) Fix 22.04 setup

--- ubuntu/22.04/modules/cuda128.sh
+++ ubuntu/22.04/modules/cuda128.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
 
-alias cc="gcc-11"
+sudo apt install -y gcc-12 g++-12
+alias cc="gcc-12"
 
 # Rermove nvidia packages
-sudo apt-mark unhold \
-cuda* \
+sudo apt-mark unhold cuda* cudnn* nvidia* libnvidia* xserver-xorg-video-nvidia* libxnvctrl*
+sudo apt purge -y --allow-change-held-packages "*cuda*" "*cudnn*" "*nvidia*" "*nsight*" "libcublas*" "libxnvctrl*"
 nvidia* \
 libnvidia* \
 xserver-xorg-video-nvidia* \
-libxnvctrl*
-
-sudo apt purge -y --allow-change-held-packages \
-"cuda*" "nvidia*" "libnvidia*" "xserver-xorg-video-nvidia*" "libxnvctrl*" "libcublas*"
-
 sudo apt autoremove -y
 sudo apt clean -y
-
-mkdir -p ~/lib/nvidia
-cd ~/lib/nvidia
+sudo apt update
+sudo apt install -f
+sudo dpkg --configure -a
 
 # Install cuda-keyring
+mkdir -p ~/lib/nvidia
+cd ~/lib/nvidia
 if [ -f "cuda-keyring_1.1-1_all.deb" ]; then
     echo "cuda-keyring_1.1-1_all.deb exists."
 else
-    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.1-1_all.deb
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
 fi
-sudo dpkg -i cuda-keyring_1.1-1_all.deb
-sudo apt update
+sudo dpkg -i cuda-keyring_1.1-1_all.deb;sudo apt update
 
 # Install libssl1.1
 sudo add-apt-repository -y ppa:nrbrtx/libssl1
@@ -35,64 +32,18 @@ sudo apt install -y libssl1.1
 
 # Install cuda
 sudo apt install -y --allow-downgrades --allow-change-held-packages \
-cuda=12.8.0-1 \
-cuda-toolkit-12-8=12.8.0-1 \
-cuda-compiler-12-8=12.8.0-1 \
-cuda-nvcc-12-8=12.8.93-1 \
-cuda-12-8=12.8.0-1 \
-cuda-runtime-12-8=12.8.0-1 \
-cuda-demo-suite-12-8=12.8.90-1 \
-cuda-toolkit-config-common=12.8.90-1 \
-cuda-toolkit-12-8-config-common=12.8.90-1 \
-libnvidia-cfg1-570=570.133.20-0ubuntu1 \
-libnvidia-common-570=570.133.20-0ubuntu1 \
-libnvidia-compute-570=570.133.20-0ubuntu1 \
-libnvidia-decode-570=570.133.20-0ubuntu1 \
-libnvidia-encode-570=570.133.20-0ubuntu1 \
-libnvidia-extra-570=570.133.20-0ubuntu1 \
-libnvidia-fbc1-570=570.133.20-0ubuntu1 \
-libnvidia-gl-570=570.133.20-0ubuntu1 \
-nvidia-compute-utils-570=570.133.20-0ubuntu1 \
-nvidia-dkms-570-open=570.133.20-0ubuntu1 \
-nvidia-driver-570-open=570.133.20-0ubuntu1 \
-nvidia-kernel-common-570=570.133.20-0ubuntu1 \
-nvidia-kernel-source-570-open=570.133.20-0ubuntu1 \
-nvidia-modprobe=570.133.20-0ubuntu1 \
-nvidia-open=570.133.20-0ubuntu1 \
-nvidia-settings=570.133.20-0ubuntu1 \
-nvidia-utils-570=570.133.20-0ubuntu1 \
-xserver-xorg-video-nvidia-570=570.133.20-0ubuntu1 \
-libxnvctrl0=570.133.20-0ubuntu1
+cuda-12-8 \
+cuda-toolkit-12-8 \
+cudnn=9.13.1-1 \
+cuda-drivers-570 \
+nvidia-driver-570 \
 
 sudo apt-mark hold \
-cuda \
-cuda-toolkit-12-8 \
-cuda-compiler-12-8 \
-cuda-nvcc-12-8 \
 cuda-12-8 \
-cuda-runtime-12-8 \
-cuda-demo-suite-12-8 \
-cuda-toolkit-config-common \
-cuda-toolkit-12-8-config-common \
-libnvidia-cfg1-570 \
-libnvidia-common-570 \
-libnvidia-compute-570 \
-libnvidia-decode-570 \
-libnvidia-encode-570 \
-libnvidia-extra-570 \
-libnvidia-fbc1-570 \
-libnvidia-gl-570 \
-nvidia-compute-utils-570 \
-nvidia-dkms-570-open \
-nvidia-driver-570-open \
-nvidia-kernel-common-570 \
-nvidia-kernel-source-570-open \
-nvidia-modprobe \
-nvidia-open \
-nvidia-settings \
-nvidia-utils-570 \
-xserver-xorg-video-nvidia-570 \
-libxnvctrl0
+cuda-toolkit-12-8 \
+cudnn \
+cuda-drivers-570 \
+nvidia-driver-570
 
 if ! grep -Fxq "## CUDA and cuDNN paths" ~/.bashrc
 then
--- ubuntu/22.04/modules/isaac_sim.md
+++ ubuntu/22.04/modules/isaac_sim.md
@@ -1,14 +1,34 @@
 # Setup isaac sim
 - Reference: [Installation using Isaac Sim Pip Package](https://isaac-sim.github.io/IsaacLab/main/source/setup/installation/pip_installation.html)
 
-# Prepare
-- Disable conda autostart (base): `conda config --set auto_activate_base false`
+# Install anaconda
+```bash
+mkdir -p ~/lib/anaconda; cd ~/lib/anaconda
+wget -O anaconda.sh https://repo.anaconda.com/archive/Anaconda3-2024.06-1-Linux-x86_64.sh
+bash anaconda.sh
+export PATH="/home/$USER/anaconda3/bin:$PATH"
+```
+- Do you accept the license terms? [yes|no] >>> `yes`
+- Press ENTER to confirm the location >>> ENTER
+- You can undo this by running `conda init --reverse $SHELL`? [yes|no] >>> `no`
+
+# Disable Conda base startup
+```conda config --set auto_activate_base false #Disable conda autostart (base)```
 
 # Setup
-- conda create -n env_isaaclab python=3.11 -y
-- pip install torch==2.7.0 torchvision==0.22....url https://download.pytorch.org/whl/cu128
-- pip install "isaacsim[all,extscache]==5.0.0.0" --extra-index-url https://pypi.nvidia.com
+```bash
+mkdir -p ~/lib/isaaclab; cd ~/lib/isaaclab
+conda create -n env_isaaclab python=3.11 -y;conda activate env_isaaclab
+pip install torch==2.7.0 torchvision==0.22.0 --index-url https://download.pytorch.org/whl/cu128
+pip install "isaacsim[all,extscache]==5.0.0" --extra-index-url https://pypi.nvidia.com
+```
+# isaac lab
+```bash
+git clone git@github.com:isaac-sim/IsaacLab.git
+```
 
-# Remove
-- conda activate base
-- conda remove --name env_isaaclab --all -y
+# Remove env_isaaclab
+```bash
+conda activate base
+conda remove --name env_isaaclab --all -y
+```
